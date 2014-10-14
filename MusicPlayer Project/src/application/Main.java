abbe59f36561a051d63422b5a662bab06315f270
ducthinhle92 <ducthinhle92@gmail.com>
2014-10-14 13:30:19 +0700

    Cập nhật dự án [14/10/2014]

--- MusicPlayer Project/src/application/Main.java
+++ MusicPlayer Project/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane) FXMLLoader
+			Parent root = (Parent) FXMLLoader
 					.load(getClass().getResource("LibraryScreen.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(
